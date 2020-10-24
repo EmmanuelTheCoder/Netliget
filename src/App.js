ff67c32cd6ef0b5609916c245f40b1e2a94d8cdf
EmmanuelTheCoder <loyaltysamuel001@gmail.com>
2020-10-24 17:59:56 +0100

    finished for now

--- src/App.js
+++ src/App.js
@@ -1,69 +1,65 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 const App = () =>{
+
+  const showResult = {
+    display: 'block',
+    marginLeft: '-4rem',
+    animation: '2s forward infinite',
+    transition: 'all 3s ease',
+    
+  }
+
   const[inputVal, setInputVal] = useState("");
   const [result, setResult] = useState("");
   const [display, setDisplay] = useState()
-
-  const hideResult = {
-    display: 'none'
-  }
-  const showResult = {
-    display: 'block',
-    transition: '3s all ease-in'
-  }
-  useEffect(()=>{
-    setDisplay(hideResult);
-    setInputVal();
-  },[hideResult]);
   
+  // const hideResultFunc = () =>{
+  //   return hideResult 
+  // }
+  // useEffect(()=>{
+  //   setDisplay(hideResultFunc);
+  // }, [hideResultFunc]);
+
   
   const handleInputChange = (e) =>{
     setInputVal(e.target.value);
-    console.log(inputVal)
+    
   }
-  const Emmanuel =  ["john", "shola", "bimbo"];
   const Mtn = ["07025", "07026", "0703", "0706", "0803", "0806", "0810", "0813", "0816", "0814", "0906", "0903"]
   const Etisalat = ["0809", "0817", "0818", "0909", "0908"];
   const Glo = ["0705", "0805", "0811", "0807","0815", "0905", "0915"];
   const Airtel = ["0708", "0802", "0808", "0812", "0701", "0901", "0902"];
   const Starcomms = ["07028", "07029", "0819"]
+ 
 
-  console.log(Emmanuel.map((emma,index)=> emma[index]))
   const handleValidity = () =>{
-    // show the result by updating the state
-    // setDisplay(showResult);
+    //show the result by updating the state
+    setDisplay(showResult);
+
     if(Mtn.includes(inputVal.slice(0, 4)) || Mtn.includes(inputVal.slice(0,5))){
-      alert("this is an mtn number");
-      setResult(`${inputVal} is an MTN number`)
+      setResult(`${inputVal} is a MTN phone number`)
     }
-    if(Etisalat.includes(inputVal.slice(0, 4))){
-      alert("this is an Etisalat number");
-      setResult(`9mobile. ${inputVal} is an 9mobile number`)
+    else if(Etisalat.includes(inputVal.slice(0, 4))){
+      setResult(`9mobile. ${inputVal} is a 9mobile phone number`)
     }
-    if(Glo.includes(inputVal.slice(0, 4))){
-      alert("this is a Glo number");
+    else if(Glo.includes(inputVal.slice(0, 4))){
       setResult(`Glo. ${inputVal} belongs to the Glo network`)
     }
-    if(Airtel.includes(inputVal.slice(0, 4))){
-      alert("this is an Airtel number");
+    else if(Airtel.includes(inputVal.slice(0, 4))){
       setResult(`Airtel. ${inputVal} belongs to the Airtel network`)
     }
-    if(Starcomms.includes(inputVal.slice(0,4)) || Starcomms.includes(inputVal.slice(0, 5))){
-      alert("this is a starcomms number");
+    else if(Starcomms.includes(inputVal.slice(0,4)) || Starcomms.includes(inputVal.slice(0, 5))){
       setResult(`Starcommms. ${inputVal} belongs to the Starcomms network`)
     }
-    if(typeof inputVal === 'string'){
-      setResult(`Please input a phone number and not a text`)
-    }
+    
     else{
       setResult(`
-      ${inputVal} does not belong to Nigeria or you have inputed a wrong number`)
+      "${inputVal}" does not belong to Nigeria or you have inputed a wrong number`)
     }
-    // setInputVal('');
+    setInputVal('');
     
-
   }
   return(
     <div className="app">
@@ -82,6 +78,7 @@ const App = () =>{
       <div className="result-container" style={display}>
       <p className="result">{result}</p>
       </div>
+      <p className="footer" style={{textAlign:'center', marginTop: '13rem', fontSize: '1.2rem', fontFamily:'Georgia'}}>loyaltysamuel001@gmail.com</p>
     </div>
   );
 }
